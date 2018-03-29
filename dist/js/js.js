0c048d3d66e93d42aa416dea3a8b492889cbed11
NelenV <vlas.lvv@gmail.com>
2018-03-29 16:59:03 +0300

    all f

---
+++ dist/js/js.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    $('a[href^="#"]').click(function(e) {
+        e.preventDefault();
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top; {
+            $('html,body').animate({ scrollTop: destination }, 1500);
+        }
+        return false;
+    });
+
+
+    (function($) {
+        $(function() {
+            $('.mobile-menu').on('click', function() {
+                $(this).closest('.header-menu').toggleClass('mobile-menu--open');
+            });
+        });
+    })(jQuery);
+
+
+});
+
+/*WORK SLIDER*/
+
+(function($) {
+    $('.works-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000
+  });
+})(jQuery);
+
+
+//TEAM SLIDER 
+
+$('.team-slider').slick({
+    dots: false,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: '-10px',
+    initialSlide: 1,
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 2,
+                centerMode: false,
+                initialSlide: 0,
+            }
+            },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1,
+                centerMode: false,
+                initialSlide: 0,
+            }
+        }]
+});
+
+
+//WOW 
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+});
+wow.init();
+
+
+// КАРТА
+function initMap() {
+    var uluru = { lat: -7.9231192, lng: 112.6003897 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru,
+        disableDefaultUI: true,
+        scrollwheel: false
+    });
+
+    var marker = new google.maps.Marker({
+        position: { lat: -7.9131192, lng: 112.6101895 },
+        map: map,
+        icon: 'img/map.png'
+    });   
+};
+
+document.addEventListener('DOMContentLoaded', initMap);
